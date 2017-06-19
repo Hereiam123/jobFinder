5a18d0c0356b58e2f0aa0f060fe260fd7015bb13
hereiam123 <bdemaio1@gmail.com>
2017-06-19 13:36:09 -0400

    Using cakephp's authorization component, currently building register and login system

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -3,24 +3,33 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\Event\Event;
 
 class UsersController extends AppController{
     public $name='Users';
+
+    //A part of the cakephp user authentication controller
+    public function beforeFilter(Event $event){
+        parent::beforeFilter($event);
+        $this->Auth->allow('register');
+    }
 
     /*
     *   Register a user
     */
     public function register(){
+
         //Register user data from register form
-        if($this->request->is('post')){
-
-            $user=$this->Users->newEntity($this->request->data);
-
-            if($this->Users->save($user)){
-                $this->Flash->set(__('Your have been registered'));
-                return $this->redirect(array('action'=>'index'));
+        $user = $this->Users->newEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity($user, $this->request->getData());
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+                return $this->redirect(array('controller'=>'jobs','action'=>'index'));
             }
+            $this->Flash->error(__('Unable to add the user.'));
         }
+        $this->set('user', $user);
 
         $this->set('title', 'Register at JobFinder');
     }
